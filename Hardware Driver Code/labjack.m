af22b78d9296f880d39cb089b68c862d2f3c2339
p-parkinson <patrick.parkinson@manchester.ac.uk>
2022-01-27 13:43:24 +0000

    Commenting labjack.m

--- Hardware Driver Code/labjack.m
+++ Hardware Driver Code/labjack.m
@@ -1,38 +1,56 @@
+%% Class to connect to LabJack U6 model via dotnet drivers
+%   Version : 0.1
+%   Author  : Patrick Parkinson (patrick.parkinson@manchester.ac.uk)
+%
+%   Usage (setting output 1 to 5V):
+%       lj = labjack();
+%       lj.voltage = 5;
+%
 classdef labjack < handle
-    properties
+    
+    properties (Access = private)
+        % Connection to labjack
         ljudObj
         ljhandle
     end
     
     properties (Dependent = true, GetAccess = private)
+        % Set-only voltage
         voltage;
     end
     
-    methods 
+    methods
         
         function obj = labjack()
+            % Initialize and connect
+            % Add dotnet assembly
             NET.addAssembly('LJUDDotNet');
+            % Get object
             obj.ljudObj = LabJack.LabJackUD.LJUD;
+            % Open/connect
             [ljerror, obj.ljhandle] = obj.ljudObj.OpenLabJackS('LJ_dtU6', 'LJ_ctUSB', '0', true, 0);
-            if ~strcmp(ljerror,'NOERROR') 
+            if ~strcmp(ljerror,'NOERROR')
                 error(ljerror);
             end
         end
         
         function set.voltage(obj,v)
-                obj.ljudObj.eDAC(obj.ljhandle, 1, v, 0, 0, 0);
-                %obj.ljudObj.eDAC(0, 0, v, 0, 0, 0);
+            % Set DAC on channel 1
+            obj.ljudObj.eDAC(obj.ljhandle, 1, v, 0, 0, 0);
         end
         
         function on(obj)
+            % Set channel 1 to 5V (TTL on)
             obj.ljudObj.eDAC(obj.ljhandle, 1, 5, 0, 0, 0);
         end
-
+        
         function off(obj)
+            % Set channel 1 to 0V (TTL off)
             obj.ljudObj.eDAC(obj.ljhandle, 1, 0, 0, 0, 0);
         end
         
         function delete(obj)
+            % Destroy object
             obj.ljudObj.Close();
         end
     end
