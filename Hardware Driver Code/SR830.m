2924fc32f7b244cf901f6e2c058076b29a5f4814
p-parkinson <patrick.parkinson@manchester.ac.uk>
2022-01-27 16:46:00 +0000

    Update SR830.m

--- Hardware Driver Code/SR830.m
+++ Hardware Driver Code/SR830.m
@@ -1,26 +1,28 @@
-    % For reading and writing to SR830 lock-in amplifier
-    % Properties include:
-        % port (i.e. serial, GPIB etc.)
-        % x,y,r,theta,aux and frequency (downloaded or calculated)
-    % Public methods include:
-        % snap() (as always)
-        % setAux(port,val)
-    % Private methods include:
-        % Constructor, Destructor
-        % Connection
+%% SR830 lock-in amplifier wrapper
+%   Version : Release
+%   Author  : Patrick Parkinson (patrick.parkinson@manchester.ac.uk)
+%
+%   This code uses a COM-port connection to the SR830 lock-in amplifier.
+%
+%   Usage - taking and displaying a value:
+%       li = SR830();
+%       x = li.x;
+%
 classdef SR830 < handle
    
     %%%%%%%%%% Properties
     properties
         port='COM15';
-    end % properties
+    end 
     
     properties (Access = private)
+        % Internal connections
         connected
         connection
-    end % connection related properties
+    end 
     
     properties (Dependent=true, SetAccess = private)
+        % Readable numbers
         frequency
         aux
         x
@@ -28,23 +30,25 @@ classdef SR830 < handle
         r
         theta
         sensitivity
-    end % properties
+    end 
     
-    %%%%%%%%%% Methods
-    
+%% Hidden methods   
     methods (Access = protected, Hidden=true)
         
         function o = query(obj,string)
+            % Send and recieve
             fprintf(obj.connection,string);
             o = fscanf(obj.connection,'%s');
         end
         
         function send(obj,string)
+            % Send a command
             fprintf(obj.connection,string);
         end
         
-    end % Private methods
+    end 
     
+    %% Public methods 
     methods
         
         function obj=SR830(port)
@@ -58,6 +62,7 @@ classdef SR830 < handle
         end
         
         function delete(obj)
+            % Clean destroy/close
             fclose(obj.connection);
             delete(obj.connection);
         end % destructor
@@ -76,6 +81,7 @@ classdef SR830 < handle
         end
         
         function value = get.aux(obj)
+            % Get all 4 auxilliary inputs
             value = zeros(4,1);
             for i=1:4
                 value(i)=double(sscanf(obj.query(['OAUX ? ',int2str(i)]),'%f'));
@@ -83,14 +89,17 @@ classdef SR830 < handle
         end
         
         function value = get.sensitivity(obj)
+            % Get lockin sensitivity
             value.n=uint8(sscanf(obj.query('SENS ?'),'%f'));
             l = [1 2 5];
             i = value.n+1;
+            % Convert
             mult = (i-mod(i,3))/3;
             value.r=l(mod(i,3)+1)*10^(-9+mult);      
         end
         
         function [o,str] = getStatus(obj)
+            % Read current lockin status
             o = uint8(sscanf(obj.query('LIAS?'),'%d'));
             str = '';
             if o>0
@@ -102,11 +111,13 @@ classdef SR830 < handle
         end
         
         function s = snap(obj)
+            % Get a simultaneous measurement of all 6 readings
             t=sscanf(obj.query('SNAP?1,2,3,4,5,6'),'%f,%f,%f,%f,%f,%f');
             s.x = t(1); s.y = t(2); s.r = t(3); s.theta = t(4); s.aux1 = t(5); s.aux2 = t(6);
         end
         
         function o = autoSnap(obj)
+            % Attempt to find an optimum sensitivity
             s = obj.getStatus();
             if bitand(s,4)
                 % Overloaded
