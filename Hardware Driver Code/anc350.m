d4b1e99b4a0f3e9d72bb01dd93df2aa77ef89004
p-parkinson <patrick.parkinson@manchester.ac.uk>
2022-01-27 13:37:52 +0000

    Update anc350 with commenting

--- Hardware Driver Code/anc350.m
+++ Hardware Driver Code/anc350.m
@@ -3,31 +3,37 @@
 %   Created : 23/09/2020
 %   Author  : Patrick Parkinson (patrick.parkinson@manchester.ac.uk)
 %
-%/Users/user/Documents/GitHub/matlab-hardware-drivers/Hardware Driver Code/ADC_dotnet.m
 %   Note, axes are defined in mm, so um is 1e-3, and nm is 1e-6.
+%
 %   Usage:
 %       atto = anc350();
 %       disp(atto.position);
 %       disp(atto.status);
-%       atto.position = [0.0 1e-3];
+%       atto.position = [0, 0, 1e-3];
 %       delete(atto);
 
 classdef anc350 < handle
-     properties (Hidden=true)
-            handle;
-            servo = [0 0 0];
-            zero  = [0 0 0];               
-     end
-     
-     properties (Dependent=true)
-         position
-     end
-     
-     properties (Dependent = true, SetAccess = private)
-         status
-         voltage
-     end
-         
+    properties (Hidden=true)
+        % Internal connection to handle
+        handle;
+        % Servo status
+        servo = [0 0 0];
+        % Stage zero position
+        zero  = [0 0 0];
+    end
+    
+    properties (Dependent=true)
+        % Dependant get/set on position
+        position
+    end
+    
+    properties (Dependent = true, SetAccess = private)
+        % Read only status
+        status
+        % Read only voltage on each stage
+        voltage
+    end
+    
     methods
         function obj=anc350()
             % Load the library. Requires anc350v4.dll, anc350res.h,
@@ -48,16 +54,18 @@ classdef anc350 < handle
             vp=libpointer('voidPtr');
             [err,~]=calllib('anc350v4','ANC_connect',0,vp);
             obj.checkerr(err);
+            % Save handle internally
             obj.handle = vp;
             fprintf(' : connected');
             % Check if connected
             [err,~,id, serNum,~,Conn]=calllib('anc350v4','ANC_getDeviceInfo',0,0,0,0,0,0);
-            obj.checkerr(err);            
+            obj.checkerr(err);
+            % Check connection
             if ~Conn
                 error('anc350:initialise:not_connected','Could not connect to device');
             end
             fprintf(' : ID %d (#%d)\n',id,serNum);
-            % Enable servos
+            % Enable servos on each stage
             for i =1:3
                 obj.set_servo(i-1,1);
                 obj.servo(i) = 1;
@@ -65,6 +73,7 @@ classdef anc350 < handle
         end
         
         function servos_on(obj)
+            % Turn all servos on, one-by-one
             for i =1:3
                 obj.set_servo(i-1,1);
                 obj.servo(i) = 1;
@@ -72,28 +81,33 @@ classdef anc350 < handle
         end
         
         function delete(obj)
-            % Clean up
+            % Clean up object
             err=calllib('anc350v4','ANC_disconnect',obj.handle);
             obj.checkerr(err);
         end
         
         function p=get.position(obj)
-            % Get 3-axis position. Note internally 0 based, MATLAB 1-based
+            % Get 3-axis position. Note internally 0 based, MATLAB is 1-based
             p = zeros(3,1);
+            % Step over each axis and get position
             for i = 1:3
                 p(i)=obj.get_position(i-1);
             end
+            % Offset using internal zero
             p = (p-obj.zero');
         end
         
         function set_voltage(obj,axis,voltage)
-            % Set a DC voltage for fine positioning mode
+            % Set a DC voltage to an axis, for fine positioning mode
             if voltage<-0.1 || voltage>60.1
                 error('anc350:set_voltage:out_of_range','Applied voltage is out of range (0V to 60V)');
             end
+            % Only allow setting 1,2,3
             if ~any(axis==[1,2,3])
                 error('anc350:set_voltage:wrong_axis','Specify axis 1,2,3');
             end
+            % Check that the servo is enabled - should not be in voltage
+            % mode
             if obj.servo(axis)
                 warning('Servo must be disabled for set_voltage mode. Disabling now.');
                 obj.set_servo(axis-1,0);
@@ -104,73 +118,87 @@ classdef anc350 < handle
         end
         
         function v=get.voltage(obj)
-           v = [0,0,0];
-           for i =1:3
+            % Read the voltages currently applied to each axis
+            v = [0,0,0];
+            % Step over each, reading
+            for i =1:3
                 [err,~,v_t]=calllib('anc350v4','ANC_getDcVoltage',obj.handle,i-1,0);
                 v(i) = v_t;
                 obj.checkerr(err);
-           end
+            end
         end
         
         function set.position(obj,p)
-            % Set the position (equivalent to move in 3D)
+            % Set the position (equivalent to move in 3D). This is three
+            % separate calls.
             for i=1:3
                 obj.set_target(i-1,p(i)+obj.zero(i));
             end
         end
         
         function set_zero(obj,posn)
-            % Set a zero position - either where we are, or with a
-            % co-ordinate system provided
+            % Set a zero position - either where we are if no position passed,
+            % or with a co-ordinate system provided
             if nargin == 1
+                % No argument, read position. We read raw here.
                 for i =1:3
                     obj.zero(i) = obj.get_position(i-1);
                 end
             else
+                % Record passed vector
                 obj.zero = posn;
             end
         end
         
         function s=get.status(obj)
             % Read the system status from all axes
-            s=struct('connected',[0 0 0],'enabled',[0 0 0],'moving',[0 0 0],'target',[0 0 0],'eotFwd',[0 0 0],'eotBwd',[0 0 0],'error',[0 0 0]);
+            s=struct('connected',[0 0 0],'enabled',[0 0 0],...
+                'moving',[0 0 0],'target',[0 0 0],'eotFwd',[0 0 0],...
+                'eotBwd',[0 0 0], 'error',[0 0 0]);
+            % Step over each axis, reading status
             for i =1:3
                 o = obj.get_status(i-1);
                 f = fieldnames(o);
+                % Assign each status
                 for j = 1:numel(f)
                     s.(f{j})(i) = o.(f{j});
                 end
             end
         end
-        
-    end 
+    end
     
-    %% Hidden methods, internal communication
+    %% Hidden methods, internal communication, not for user use
     methods (Hidden= true)
         
         function set_target(obj,axis,target)
+            % Set target position for an axis. Doesn't start a move - servo
+            % follows this target.
+            
             % Check if servo on
             if ~obj.servo(axis+1)
                 warning('anc350:set_target:servo_off','Turn on the servo before setting a target');
             end
+            
             % Note, internally in mm. Convert to m.
             target = target/1000;
+            % Set target
             err=calllib('anc350v4','ANC_setTargetPosition',obj.handle,axis,target);
             obj.checkerr(err);
         end
-            
+        
         
         function set_servo(obj,axis,state)
-            % Turn servo for targetting
+            % Change servo status for targetting
             if and(state ~=0,state ~=1)
                 error('anc350:set_servo:unknown_state','State must be 0 (off) or 1 (on)');
             end
-            % Start automove
+            % Set servo status
             err=calllib('anc350v4','ANC_startAutoMove',obj.handle,axis,state,1);
             obj.checkerr(err);
             % Record
             obj.servo(axis+1) = state;
-            % Set to current position (stop immediate movement!)
+            % Set to current position (stop immediate movement!) This is
+            % useful to stop the servo flying off.
             if state == 1
                 obj.set_target(axis,obj.get_position(axis));
             end
@@ -186,13 +214,18 @@ classdef anc350 < handle
         
         function s = get_status(obj,axis)
             % Get the status of an axis
-            [err,~,connected,enabled,moving,target,eotFwd,eotBwd,error] = calllib('anc350v4','ANC_getAxisStatus', obj.handle,axis,0,0,0,0,0,0,0);
+            [err,~,connected,enabled,moving,target,eotFwd,eotBwd,error] =...
+                calllib('anc350v4','ANC_getAxisStatus', obj.handle, axis, ...
+                0,0,0,0,0,0,0);
             obj.checkerr(err);
-            s = struct('connected',connected,'enabled',enabled,'moving',moving,'target',target,'eotFwd',eotFwd,'eotBwd',eotBwd,'error',error);
+            % Return a structure
+            s = struct('connected',connected,'enabled',enabled,'moving',...
+                moving,'target',target,'eotFwd',eotFwd,'eotBwd',eotBwd,...
+                'error',error);
         end
         
         function checkerr(~,err)
-            % Check the error codes against those defined
+            % Check the error codes against those defined in the manual
             switch err
                 case 0
                     % No error
